7a299a6b8efb038775862358ba752ca76bdb6655
Oleksandr Korienev <alexkorienev@gmail.com>
2018-09-09 12:34:10 +0300

    lab1

---
+++ Lab1/src/main/java/com/lab111/aTestMain.java
@@ -0,0 +1,16 @@
+package com.lab111;
+
+/**
+ * Test class to check task
+ */
+public class aTestMain {
+    /**
+     * Test method which returns name and value of given parameter
+     * @param value Integer value of parameter
+     * @param name Name of parameter
+     * @return formatted string
+     */
+    String function(int value, String name){
+        return "integer parameter with name " + name + " and value " + value;
+    }
+}
